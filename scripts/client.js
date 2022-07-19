94c68ad3ebfe92975485ae67a1fe35716911cc76
Halima Moalim <halimaamoalim@gmail.com>
2022-07-19 16:39:46 -0500

    fixing empty emplyee array

--- scripts/client.js
+++ scripts/client.js
@@ -5,4 +5,21 @@ function onReady() {
     $('#employeeList').on('click', '.delete', removeEmployee);
 }
 
+let employeeList = [];
+
+function addEmployee(event) {
+    // this prevents the form from refreshing the page on submit
+    event.preventDefault();
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        id: $('#idIn').val(),
+        lastName: $('#lastNameIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val(),
+    }
+    //adding the employees to the DOM with isolated variable 
+    employeeList.push(newEmployee);
+    $('.employeeInput').val('');
+    showEmployee();
+}
 
